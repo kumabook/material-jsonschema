a4d926faa74304aaf9733ec7e6182b3737be0dc4
Hiroki Kumamoto <kumabook@live.jp>
2017-05-28 20:47:58 +0900

    Bump version up to 0.0.8

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var Form  = require('./lib/Form').default;
-var Table = require('./lib/Table').default;
-var createResourceActions = require('./lib/createResourceActions').default;
+const Form  = require('./lib/Form').default;
+const Table = require('./lib/Table').default;
+const createResourceActions = require('./lib/createResourceActions').default;
 
 module.exports = {
-  Form:  Form,
-  Table: Table,
-  createResourceActions: createResourceActions,
+  Form,
+  Table,
+  createResourceActions,
 };
